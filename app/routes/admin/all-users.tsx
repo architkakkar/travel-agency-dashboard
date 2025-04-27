8b642f4a312a49ad4abaf1a9e7fdce52b0cf5f45
Archit Kakkar <education.archit@gmail.com>
2025-04-28 00:52:43 +0530

    sidebar and header part completed.

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,5 +1,15 @@
+import { Header } from "~/components";
+
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+      All users page content
+    </main>
+  );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,18 @@
+import { Header } from "~/components";
+
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const user = {
+    name: "David Smith",
+  };
+  return (
+    <main className="dashboard wrapper">
+      <Header 
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard page content
+    </main>
+  );
 };
 
 export default Dashboard;
